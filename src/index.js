d0c2f7a97e7a9765127e6ed1ae0fbc02bc809239
Didier <115608922+DogGotSwag@users.noreply.github.com>
2025-05-01 00:19:42 -0600

    load in header/footer images through js file
    this is so they showup the in live versions since I think
    webpack can not find them normally

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import './style.css';
+import headerImage from '../img/pexels-charlesdeluvio-1851164.jpg';
+import FooterImage from '../img/footerPic.jpg';
+
+const imgElement = document.querySelector('.imgContainer img');
+const imgFoooterElement = document.querySelector('.footerPicBox img');
+imgElement.src = headerImage;
+imgFoooterElement.src = FooterImage;
